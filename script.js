aed8994f6841ef4691cd0050e5a712a0b3f480cd
ellen cristine <iellenctn@gmail.com>
2025-11-15 18:44:11 -0300

    inclusao de secoes e informacoes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// Mostrar seções
 function showSection(id) {
-  document.querySelectorAll('section').forEach(sec => sec.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
+  document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+
+  // Fecha menu mobile ao clicar
+  document.querySelector(".nav-links").classList.remove("active");
+}
+
+// Abrir/fechar menu mobile
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
 }
