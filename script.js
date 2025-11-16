733c2a7f8bfa160b5f2e61c72fd267a6e5353dc8
ellen cristine <iellenctn@gmail.com>
2025-11-16 19:12:11 -0300

    feat: efeito parallax, inclusao de imagens

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function showSection(id) {
 function toggleMenu() {
   document.querySelector(".nav-links").classList.toggle("active");
 }
+
+// PARALLAX DO BANNER
+
+window.addEventListener("scroll", function() {
+  const s = window.scrollY;
+
+  document.querySelector(".bg").style.transform =
+    `translateY(${s * 0.15}px)`;
+
+  document.querySelector(".mid").style.transform =
+    `translateY(${s * 0.10}px)`;
+
+  document.querySelector(".front").style.transform =
+    `translateY(${s * 0.05}px)`;
+});
