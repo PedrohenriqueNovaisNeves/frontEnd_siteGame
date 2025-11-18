1df34fdafd9476bf30d0d8965e74d0f0b3cf4e40
ellen cristine <iellenctn@gmail.com>
2025-11-18 12:34:31 -0300

    personalizacao e estilizacao

--- script.js
+++ script.js
@@ -26,3 +26,10 @@ window.addEventListener("scroll", function() {
   document.querySelector(".front").style.transform =
     `translateY(${s * 0.05}px)`;
 });
+
+document.querySelectorAll(".menu-item").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".menu-item").forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
